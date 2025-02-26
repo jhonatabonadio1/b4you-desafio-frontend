9a8acb7845c83e23b6d73ca3527e80bf9014b485
Jhonata <jhonbonadio@gmail.com>
2025-02-26 12:55:25 -0300

    .

--- src/components/files/file-card.tsx
+++ src/components/files/file-card.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { Icons } from "../icons";
 import { Badge } from "../ui/badge";
@@ -65,6 +65,8 @@ export function UploadFileCard({ file, onDelete }: Props) {
     return formattedDate;
   }
 
+  
+
   return (
     <Card
       className={`${
@@ -90,7 +92,7 @@ export function UploadFileCard({ file, onDelete }: Props) {
         <>
           <CardHeader className="flex flex-col  w-full">
             <div className="flex flex-1 overflow-auto flex-row items-center justify-between gap-2">
-              <div className="truncate flex-1 max-w-xs lg:max-w-md min-w-0l">
+              <div className={`truncate flex-1 max-w-[100px] lg:max-w-md min-w-0l`}>
                 {file.title}
               </div>
 
