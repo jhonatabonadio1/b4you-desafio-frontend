b3d1d03a825f79477f625dd3b6480acb87a20133
Jhonata <jhonbonadio@gmail.com>
2025-02-26 12:22:32 -0300

    ajustes

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,40 +1,46 @@
-import { MainNav } from "@/components/main-nav"
-import { MobileNav } from "@/components/mobile-nav"
-import { ModeSwitcher } from "@/components/mode-switcher"
-import { Button } from "./ui/button"
-import { useRouter } from "next/router"
-
+import { MainNav } from "@/components/main-nav";
+import { MobileNav } from "@/components/mobile-nav";
+import { ModeSwitcher } from "@/components/mode-switcher";
+import { Button } from "./ui/button";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
 
 export function SiteHeader() {
   const route = useRouter();
 
-  
+  const { isAuthenticated } = useContext(AuthContext);
   return (
     <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container-wrapper">
         <div className="container flex h-14 justiyitems-center">
-          
           <MainNav />
           <MobileNav />
           <div className="flex flex-1 items-center justify-end gap-2 md:justify-end">
-           
-            <nav className="flex items-center gap-2">
-              <Button
-                 onClick={() => route.push("/signup")}
-              >
-                Cadastre-se
-              </Button>
-              <Button
-                variant="outline"
-                onClick={() => route.push("/login")}
+            {isAuthenticated ? (
+              <nav className="flex items-center gap-2">
+                <Button
+                  variant="outline"
+                  onClick={() => route.push("/documents")}
                 >
+                  Meu dashboard
+                </Button>
+                <ModeSwitcher />
+              </nav>
+            ) : (
+              <nav className="flex items-center gap-2">
+                <Button onClick={() => route.push("/signup")}>
+                  Cadastre-se
+                </Button>
+                <Button variant="outline" onClick={() => route.push("/login")}>
                   Login
                 </Button>
-              <ModeSwitcher />
-            </nav>
+                <ModeSwitcher />
+              </nav>
+            )}
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
